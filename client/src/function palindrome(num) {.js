d9d774f32e7ace3a1124950565159d2454a8e260
SaraElwakill <saraelwakil2021@icloud.com>
2022-04-14 13:35:09 +0200

    redux

--- client/src/function palindrome(num) {.js
+++ client/src/function palindrome(num) {.js
@@ -1,23 +1,28 @@
-function palindrome(num) {
-    let arr = num.toString().split("")
-    let n = arr.length-1
-    let normal
-    let reverse;
-  if (num<0 || typeof(num)!=="number") return "Not valid"
+/** @format */
 
-    for(let j = 3 ; j<=n ; j++){
-        for(let i=0 ; i<arr.length-j-1 ; i++) {
-            normal = +(arr.slice(i,i+j).join(""))
-            console.log(normal);
-            reverse = +(arr.slice(i,i+j).reverse().join(""))
-            console.log(reverse);
-            if(reverse===normal) return true
-            else continue
-        }
-    }
-return false
-  }
-  console.log(palindrome(63223321));
+function palindrome(num) {
+	let arr = num.toString().split("");
+	let n = arr.length - 1;
+	let normal;
+	let reverse;
+	if (num < 0 || typeof num !== "number") return "Not valid";
+
+	for (let j = 3; j <= n; j++) {
+		for (let i = 0; i < arr.length - j - 1; i++) {
+			normal = +arr.slice(i, i + j).join("");
+			console.log(normal);
+			reverse = +arr
+				.slice(i, i + j)
+				.reverse()
+				.join("");
+			console.log(reverse);
+			if (reverse === normal) return true;
+			else continue;
+		}
+	}
+	return false;
+}
+console.log(palindrome(63223321));
 //123322367
 
 //   let test = [1,2,3,4,5,6,7]
@@ -25,3 +30,6 @@ return false
 //       console.log(test.slice(i,i+3));
 //   }
 //   console.log(test.slice(1,4));
+//check slice length before test palindrom so less than three ignored characters
+//also slice from start  and from end and enter to inside
+//(0,length - 1)-(1,length - 2)-(2,length - 3)-(3,length - 4)...
---
+++ client/src/store/usersSlice.js
@@ -0,0 +1,10 @@
+/** @format */
+
+// const login = async (req, res, dispatch) => {
+// 	axios.get("http://localhost:5000/api/users/login", (req, res) => {
+// 		data: {
+//             email: "123@123",
+//             password: "123456"
+//         }
+// 	});
+// };
