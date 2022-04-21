e400854140e9deeca6a8f99d2983f9bdd144627e
SaraElwakill <saraelwakil2021@icloud.com>
2022-04-21 17:30:47 +0200

    update

--- Server/models/Product.js
+++ Server/models/Product.js
@@ -1,20 +1,22 @@
+/** @format */
+
 const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true, unique: true },
-    description: { type: String, required: true },
-    image: [
-      {
-        type: String,
-        required: true,
-      },
-    ],
-    categories: { type: Array },
-    size: { type: String },
-    color: { type: String },
-    price: { type: Number, required: true },
-    quantity: { type: Number, required: true },
-  },
-  { timestamps: true }
+	{
+		title: { type: String, required: true },
+		description: { type: String, required: true },
+		image: [
+			{
+				type: String,
+				required: true,
+			},
+		],
+		categories: { type: Array },
+		size: { type: String },
+		color: { type: String },
+		price: { type: Number, required: true },
+		quantity: { type: Number, required: true },
+	},
+	{ timestamps: true }
 );
 module.exports = mongoose.model("Product", ProductSchema);
