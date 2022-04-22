6dd9ef93a72b88eb5f7aebbff15511a4498fe0ab
karimyassser <karimyassser@gmail.com>
2022-04-22 05:12:46 +0200

    updates

--- models/Product.js
+++ models/Product.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD:models/Product.js
-const mongoose = require("mongoose")
-const ProductSchema = new mongoose.Schema({
-    title: { type: String, required: true, unique: true },
-    description: { type: String, required: true },
-    image: { type: String, required: true },
-    categories: { type: Array },
-    size: { type: String },
-    color: { type: String },
-    price: { type: Number, required: true },
-    quantity: { type: Number, required: true },
-}, { timestamps: true })
-module.exports = mongoose.model("Product", ProductSchema)
-=======
 /** @format */
 
 const mongoose = require("mongoose");
@@ -42,4 +28,3 @@ const ProductSchema = new mongoose.Schema(
 	{ timestamps: true }
 );
 module.exports = mongoose.model("Product", ProductSchema);
->>>>>>> 643f79d3963dca8413de8660110c63f182b09107:Server/models/Product.js
--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:models/User.js
 const mongoose = require("mongoose")
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
@@ -11,18 +10,4 @@ const UserSchema = new mongoose.Schema({
     isActive: {type:Boolean},
     status:{type: String , default: "active" }
 }, { timestamps: true })
-=======
-const mongoose = require("mongoose")
-const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, unique: true },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
-    isActive: {type:Boolean},
-    status:{type: String , default: "active" }
-}, { timestamps: true })
->>>>>>> 643f79d3963dca8413de8660110c63f182b09107:Server/models/User.js
 module.exports = mongoose.model("User",UserSchema)
