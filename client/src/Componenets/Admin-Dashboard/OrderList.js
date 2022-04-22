98039f90bb52b5f7664525c2e85087a0ca7d40ba
karimyassser <karimyassser@gmail.com>
2022-04-22 04:52:50 +0200

    https://github.com/sammwafy/sprints-nodeJS-ecommerce.git

--- client/src/Componenets/Admin-Dashboard/OrderList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const OrderList = () => {
-  return (
-    <div>OrderList</div>
-  )
-}
-
-export default OrderList
---
+++ routes/cart.js
@@ -0,0 +1,52 @@
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken")
+const Cart = require("../models/Cart")
+const router = require("express").Router()
+//CREATE
+router.post("/", verifyToken, async (req, res) => {
+    const newCart = new Product(req.body)
+    try {
+        const savedCart = await newCart.save()
+        res.status(200).json(savedCart)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true })
+        res.status(200).json(updatedCart)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await Cart.findByIdAndDelete(req.params.id)
+        res.status(200).json("cart has been deleted")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+//GET USER CART
+router.get("/find/:id", verifyTokenAndAuthorization,async (req, res) => {
+    try {
+        const cart = await Cart.findOne({userID:req.params.id})
+        res.status(200).json(cart)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+//GET ALL CARTS
+router.get("/",verifyTokenAndAdmin ,async (req,res)=>{
+    try{
+        const carts =  await Cart.find()
+        res.status(200).json(carts)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+module.exports = router
