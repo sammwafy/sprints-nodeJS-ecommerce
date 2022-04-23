41fbe8add02abfd1043a637301a3500b6f990c8e
shaimaayosef <shimaa.sayed8888@gmail.com>
2022-04-23 03:53:40 +0200

    ..

--- client/src/Pages/homepage/HomePage.js
+++ client/src/Pages/homepage/HomePage.js
@@ -91,7 +91,6 @@ const HomePage = () => {
             />
           </Container>
         </TestimonialsContainter>
-        >>>>>>> 1f829d543b7e5a3b5e7fedeca83518703b6bf973
         <Carousel type="brands" />
       </Layout>
     </>
