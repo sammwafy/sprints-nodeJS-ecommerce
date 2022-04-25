b41289d9bf814bcc53109c38eeeba6f759506720
Sameh Hassan <dent.semo@gmail.com>
2022-04-25 20:36:25 +0200

    axios instance updated

--- client/src/Hooks/axios.js
+++ client/src/Hooks/axios.js
@@ -3,5 +3,12 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: "https://sprints-ecommerce-server.herokuapp.com/",
+  baseURL: "https://sprints-ecommerce-server.herokuapp.com/",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers":
+      "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length",
+  },
 });
