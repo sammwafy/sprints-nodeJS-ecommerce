7385cebe23108e4c9570d7b37a91a315ec458687
sammwafy <59634544+sammwafy@users.noreply.github.com>
2022-04-28 07:49:52 +0200

    Update Rating.js

--- client/src/Componenets/ProductView/productItems/Rating.js
+++ client/src/Componenets/ProductView/productItems/Rating.js
@@ -1,10 +1,28 @@
 import Rater from "react-rater";
 import "react-rater/lib/react-rater.css";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../../../Hooks/axios.js";
 
-const Rating = () => {
-  return <Rater total={5} rating={2} interactive={false}/>;
+const Rating = ({ id }) => {
+	const [review, setReview] = useState(0);
+
+	useEffect(() => {
+		if (id) {
+			const getReview = async () => {
+				try {
+					const res = await axios.post(`/api/products/average/${id}`);
+
+					setReview(res?.data[0]?.average);
+				} catch (err) {
+					console.log(err);
+				}
+			};
+			getReview();
+		}
+	}, [id]);
+	console.log(review);
+	return <Rater total={5} rating={review || 1} interactive={false} />;
 };
 
 export default Rating;
