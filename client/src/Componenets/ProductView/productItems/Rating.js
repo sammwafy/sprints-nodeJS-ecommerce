b020fbfaeec734d0c46819749dd00c2b4f33a8a8
sammwafy <59634544+sammwafy@users.noreply.github.com>
2022-04-28 08:27:13 +0200

    Update Rating.js

--- client/src/Componenets/ProductView/productItems/Rating.js
+++ client/src/Componenets/ProductView/productItems/Rating.js
@@ -13,7 +13,7 @@ const Rating = ({ id }) => {
 			const getReview = async () => {
 				try {
 					const res = await axios.post(`/api/products/average/${id}`);
-	console.log(res?.data);
+
 					setReview(res?.data[0]?.average);
 				} catch (err) {
 					console.log(err);
