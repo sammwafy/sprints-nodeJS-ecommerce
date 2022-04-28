3e59a9379c4f88a04278b1d3c9dfa80713c45e31
sammwafy <59634544+sammwafy@users.noreply.github.com>
2022-04-28 08:17:10 +0200

    Update Rating.js

--- client/src/Componenets/ProductView/productItems/Rating.js
+++ client/src/Componenets/ProductView/productItems/Rating.js
@@ -9,20 +9,20 @@ const Rating = ({ id }) => {
 	const [review, setReview] = useState(0);
 
 	useEffect(() => {
-		if (id) {
+		
 			const getReview = async () => {
 				try {
 					const res = await axios.post(`/api/products/average/${id}`);
-
+	console.log(res?.data);
 					setReview(res?.data[0]?.average);
 				} catch (err) {
 					console.log(err);
 				}
 			};
 			getReview();
-		}
-	}, []);
-	console.log(review);
+		
+	}, [id]);
+
 	return <Rater total={5} rating={review || 1} interactive={false} />;
 };
 
