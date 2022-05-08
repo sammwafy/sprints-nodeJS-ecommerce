7b0a72c851598b02597cbf9015c2d9195fe261d7
sammwafy <59634544+sammwafy@users.noreply.github.com>
2022-05-08 06:02:39 +0200

    Update axios.js

--- client/src/Hooks/axios.js
+++ client/src/Hooks/axios.js
@@ -3,10 +3,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5009",
+  baseURL: "https://sprints-ecommerce-server.herokuapp.com",
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Origin": "https://sprints-node-js-ecommerce.vercel.app/",
     "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers":
       "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length",
