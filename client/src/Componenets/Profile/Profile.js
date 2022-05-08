5e73e9de3d9a8311e8701c3ff35cf81e13bed793
Sameh Hassan <dent.semo@gmail.com>
2022-05-09 01:44:07 +0200

    update dependency of profile axios

--- client/src/Componenets/Profile/Profile.js
+++ client/src/Componenets/Profile/Profile.js
@@ -22,9 +22,8 @@ const Profile = () => {
   const [EditAvatar, setEditAvatar] = useState(false);
   const [EditPassword, setEditPassword] = useState(false);
   const [userInfo, setUserInfo] = useState(null);
-console.log(auth)
+
   useEffect(() => {
-    if(auth.id){
     const config = {
       headers: {
         token: `Bearer ${auth?.token}`,
@@ -46,100 +45,100 @@ console.log(auth)
         console.log(err);
       }
     );
-  }
   }, [auth.id]);
 
-console.log(auth)
-console.log(userInfo)
+  console.log(auth);
+  console.log(userInfo);
   return (
-    userInfo  && 
-    userInfo.status === 'active' &&
-    <ProfileWrapper>
-      <Container>
-        {EditAboutMe ? (
-          <AboutMe
-            id={auth?.id}
-            token={auth?.token}
-            setEditAboutMe={setEditAboutMe}
-          />
-        ) : EditAvatar ? (
-         <Avatar
-         id={auth?.id}
-         token={auth?.token}
-         setEditAvatar={setEditAvatar}
-         />
-        ) : EditSocials ? (
-          <Socials
-          id={auth?.id}
-          token={auth?.token}
-          setEditSocials={setEditSocials}
-          socials={userInfo.socials}
-        />
-        ) : EditPassword ? (
-          <ChangePassword 
-          setEditPassword={setEditPassword}
-          id={auth?.id}
-          token={auth?.token}
-          />
-        ) : (
-          <>
-            <div className="avatar">
-              <Image src={userInfo?.avatar} />
-              <FaEdit
-                className="editAvatar"
-                onClick={() => setEditAvatar(true)}
-              />
-            </div>
-            <div className="userInfo">
-              <div className="infoItem">
-                <div>Username :</div> <span>{userInfo.username}</span>
+    userInfo &&
+    userInfo.status === "active" && (
+      <ProfileWrapper>
+        <Container>
+          {EditAboutMe ? (
+            <AboutMe
+              id={auth?.id}
+              token={auth?.token}
+              setEditAboutMe={setEditAboutMe}
+            />
+          ) : EditAvatar ? (
+            <Avatar
+              id={auth?.id}
+              token={auth?.token}
+              setEditAvatar={setEditAvatar}
+            />
+          ) : EditSocials ? (
+            <Socials
+              id={auth?.id}
+              token={auth?.token}
+              setEditSocials={setEditSocials}
+              socials={userInfo.socials}
+            />
+          ) : EditPassword ? (
+            <ChangePassword
+              setEditPassword={setEditPassword}
+              id={auth?.id}
+              token={auth?.token}
+            />
+          ) : (
+            <>
+              <div className="avatar">
+                <Image src={userInfo?.avatar} />
+                <FaEdit
+                  className="editAvatar"
+                  onClick={() => setEditAvatar(true)}
+                />
               </div>
-              <div className="infoItem">
-                <div>Email :</div> <span> {userInfo.email}</span>
+              <div className="userInfo">
+                <div className="infoItem">
+                  <div>Username :</div> <span>{userInfo.username}</span>
+                </div>
+                <div className="infoItem">
+                  <div>Email :</div> <span> {userInfo.email}</span>
+                </div>
+                <hr />
+                <h3>
+                  About Me <FaEdit onClick={() => setEditAboutMe(true)} />
+                </h3>
+                <p>{userInfo?.aboutMe || "no bio text yet"}</p>
+                <hr />
+                <h3>
+                  Social Links <FaEdit onClick={() => setEditSocials(true)} />
+                </h3>
+                <Row className="socialLinks">
+                  <Col>
+                    <a href={userInfo?.socials?.facebook}>
+                      <FaFacebook />
+                    </a>
+                  </Col>
+                  <Col>
+                    <a href={userInfo?.socials?.twitter}>
+                      <FaTwitter />
+                    </a>
+                  </Col>
+                  <Col>
+                    <a href={userInfo?.socials?.telegram}>
+                      <FaTelegram />
+                    </a>
+                  </Col>
+                  <Col>
+                    <a href={userInfo?.socials?.instagram}>
+                      <FaInstagram />
+                    </a>
+                  </Col>
+                </Row>
               </div>
               <hr />
-              <h3>
-                About Me <FaEdit onClick={() => setEditAboutMe(true)} />
-              </h3>
-              <p>{userInfo?.aboutMe || "no bio text yet"}</p>
-              <hr />
-              <h3>
-                Social Links <FaEdit onClick={() => setEditSocials(true)} />
-              </h3>
-              <Row className="socialLinks">
-                <Col>
-                  <a href={userInfo?.socials?.facebook}>
-                    <FaFacebook />
-                  </a>
-                </Col>
-                <Col>
-                  <a href={userInfo?.socials?.twitter}>
-                    <FaTwitter />
-                  </a>
-                </Col>
-                <Col>
-                  <a href={userInfo?.socials?.telegram}>
-                    <FaTelegram />
-                  </a>
-                </Col>
-                <Col>
-                  <a href={userInfo?.socials?.instagram}>
-                    <FaInstagram />
-                  </a>
-                </Col>
-              </Row>
-            </div>
-            <hr />
-            <div className="d-flex justify-content-between">
-              <p style={{fontWeight: '600'}}>need to change password ?</p>
-              <Button variant="warning" onClick={() => setEditPassword(true)}>
-                change password
-              </Button>
-            </div>
-          </>
-        )}
-      </Container>
-    </ProfileWrapper>
+              <div className="d-flex justify-content-between">
+                <p style={{ fontWeight: "600" }}>need to change password ?</p>
+                <Button variant="warning" onClick={() => setEditPassword(true)}>
+                  change password
+                </Button>
+              </div>
+            </>
+          )}
+        </Container>
+      </ProfileWrapper>
+    )
   );
 };
 
