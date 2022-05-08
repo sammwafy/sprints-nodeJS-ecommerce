09cd8f9c8beec5d0ade1e6bbb79e523713278fb1
Sameh Hassan <dent.semo@gmail.com>
2022-05-08 06:45:39 +0200

    return to local dev

--- client/src/Hooks/axios.js
+++ client/src/Hooks/axios.js
@@ -3,10 +3,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://secure-brushlands-98723.herokuapp.com",
+  baseURL: "http://localhost:5009",
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "https://sprints-node-js-ecommerce.vercel.app/",
+    "Access-Control-Allow-Origin": "http://localhost:3000",
     "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers":
       "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length",
